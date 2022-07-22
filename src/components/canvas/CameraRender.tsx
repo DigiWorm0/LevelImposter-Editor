bb833c31c08977c4ad22cabae2763ba22ea1178b
Digi <aus343@gmail.com>
2022-07-22 02:48:02 -0500

    Fixed Camera Aspect Ratio

--- src/components/canvas/CameraRender.tsx
+++ src/components/canvas/CameraRender.tsx
@@ -2,7 +2,8 @@ import { Rect } from "react-konva";
 import { useSelectedElemValue } from "../../hooks/jotai/useSelectedElem";
 
 const UNITY_SCALE = 100;
-const CAM_SIZE = 6; // TODO: Get the actual size of the camera
+const CAM_SIZE = 6;
+const CAM_ASPECT = 1.5;
 
 export default function CameraRender() {
     const selectedElem = useSelectedElemValue();
@@ -13,16 +14,18 @@ export default function CameraRender() {
 
     const { camXOffset, camYOffset, camZoom } = selectedElem.properties;
 
-    const camSize = (camZoom ? camZoom * 2 : CAM_SIZE) * UNITY_SCALE;
+    const camHeight = (camZoom ? camZoom * 2 : CAM_SIZE) * UNITY_SCALE;
+    const camWidth = camHeight * CAM_ASPECT;
+
     const camX = camXOffset ? camXOffset * UNITY_SCALE : 0;
     const camY = camYOffset ? -camYOffset * UNITY_SCALE : 0;
 
     return (
         <Rect
-            x={camX - camSize / 2}
-            y={camY - camSize / 2}
-            width={camSize}
-            height={camSize}
+            x={camX - camWidth / 2}
+            y={camY - camHeight / 2}
+            width={camWidth}
+            height={camHeight}
             fill="#00ff0022"
             stroke="green"
             strokeWidth={10}
