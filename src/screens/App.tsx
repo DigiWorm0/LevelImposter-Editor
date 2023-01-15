7ebd5f55dd17b121ca801a514ead79e244827e36
Digi <aus343@gmail.com>
2023-01-15 17:31:55 -0600

    Fix OnBeforeUnload Bug

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -32,7 +32,7 @@ export default function App() {
         return () => {
             window.removeEventListener("beforeunload", onBeforeUnload);
         }
-    }, []);
+    }, [isEmbeded]);
 
     React.useEffect(() => {
         if (settings.language === "auto")
