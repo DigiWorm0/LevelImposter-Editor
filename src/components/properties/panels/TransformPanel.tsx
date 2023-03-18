ae7decb778c5e623428ea969b7ea705b3184c47b
Digi <aus343@gmail.com>
2023-03-18 09:37:46 -0500

    Group Transform Panel

--- src/components/properties/panels/TransformPanel.tsx
+++ src/components/properties/panels/TransformPanel.tsx
@@ -21,12 +21,14 @@ export default function TransformPanel() {
         return getElemVisibility(selectedElem);
     }, [selectedElem]);
 
-    if (!selectedElem || selectedElem.type === "util-layer")
+    if (!selectedElem)
         return null;
 
     return (
         <>
-            <PanelContainer title={t("transform.title") as string} style={{ paddingTop: 0 }}>
+            <PanelContainer
+                title={t("transform.title") as string}
+                style={{ paddingTop: 0 }}>
                 {settings.isDevMode ? (
                     <InputGroup
                         key={selectedElem.id + "-type-dev"}
@@ -58,106 +60,108 @@ export default function TransformPanel() {
                         setSelectedElem({ ...selectedElem, name: e.target.value });
                     }}
                 />
-                <ControlGroup fill>
-                    <NumericInput
-                        key={selectedElem.id + "-x"}
-                        defaultValue={selectedElem.x}
-                        onValueChange={(val) => {
-                            !isNaN(val) && setSelectedElem({ ...selectedElem, x: val });
-                        }}
-                        fill
-                        placeholder={t("transform.x") as string}
-                        minorStepSize={0.001}
-                        stepSize={0.1}
-                        majorStepSize={1}
-                    />
-                    <NumericInput
-                        key={selectedElem.id + "-y"}
-                        defaultValue={selectedElem.y}
-                        onValueChange={(val) => {
-                            !isNaN(val) && setSelectedElem({ ...selectedElem, y: val });
-                        }}
-                        fill
-                        placeholder={t("transform.y") as string}
-                        minorStepSize={0.001}
-                        stepSize={0.1}
-                        majorStepSize={1}
-                    />
-                    <NumericInput
-                        key={selectedElem.id + "-z"}
-                        defaultValue={selectedElem.z}
-                        onValueChange={(val) => {
-                            !isNaN(val) && setSelectedElem({ ...selectedElem, z: val });
-                        }}
-                        fill
-                        placeholder={t("transform.z") as string}
-                        minorStepSize={0.001}
-                        stepSize={0.1}
-                        majorStepSize={1}
-                    />
-                </ControlGroup>
-                <ControlGroup fill>
-                    <NumericInput
-                        key={selectedElem.id + "-xScale"}
-                        defaultValue={selectedElem.xScale}
-                        onValueChange={(val) => {
-                            !isNaN(val) && setSelectedElem({ ...selectedElem, xScale: val });
-                        }}
-                        fill
-                        leftIcon="arrows-horizontal"
-                        placeholder={t("transform.xScale") as string}
-                        minorStepSize={0.01}
-                        stepSize={0.1}
-                        majorStepSize={1}
-                    />
-                    <NumericInput
-                        key={selectedElem.id + "-yScale"}
-                        defaultValue={selectedElem.yScale}
-                        onValueChange={(val) => {
-                            !isNaN(val) && setSelectedElem({ ...selectedElem, yScale: val });
-                        }}
-                        fill
-                        leftIcon="arrows-vertical"
-                        placeholder={t("transform.yScale") as string}
-                        minorStepSize={0.01}
-                        stepSize={0.1}
-                        majorStepSize={1}
-                    />
-                </ControlGroup>
-                <ControlGroup fill>
-                    <NumericInput
-                        key={selectedElem.id + "-rotation"}
-                        defaultValue={selectedElem.rotation}
-                        onValueChange={(val) => {
-                            !isNaN(val) && setSelectedElem({ ...selectedElem, rotation: val });
-                        }}
-                        fill
-                        leftIcon="refresh"
-                        placeholder={t("transform.rotation") as string}
-                        minorStepSize={1}
-                        stepSize={45}
-                        majorStepSize={90}
-                    />
-                </ControlGroup>
-                <ButtonGroup minimal style={{ marginTop: 10 }} fill>
-                    <Button
-                        fill
-                        icon={selectedElem.properties.isLocked ? "lock" : "unlock"}
-                        text={selectedElem.properties.isLocked ? t("transform.unlock") : t("transform.lock")}
-                        onClick={() => {
-                            setSelectedElem({ ...selectedElem, properties: { ...selectedElem.properties, isLocked: !selectedElem.properties.isLocked } });
-                        }}
-                    />
-                    <Button
-                        fill
-                        icon="trash"
-                        text={t("transform.delete") as string}
-                        onClick={() => {
-                            removeElement(selectedElem.id);
-                            setSelectedID("" as GUID)
-                        }}
-                    />
-                </ButtonGroup>
+                {selectedElem.type !== "util-layer" && (<>
+                    <ControlGroup fill>
+                        <NumericInput
+                            key={selectedElem.id + "-x"}
+                            defaultValue={selectedElem.x}
+                            onValueChange={(val) => {
+                                !isNaN(val) && setSelectedElem({ ...selectedElem, x: val });
+                            }}
+                            fill
+                            placeholder={t("transform.x") as string}
+                            minorStepSize={0.001}
+                            stepSize={0.1}
+                            majorStepSize={1}
+                        />
+                        <NumericInput
+                            key={selectedElem.id + "-y"}
+                            defaultValue={selectedElem.y}
+                            onValueChange={(val) => {
+                                !isNaN(val) && setSelectedElem({ ...selectedElem, y: val });
+                            }}
+                            fill
+                            placeholder={t("transform.y") as string}
+                            minorStepSize={0.001}
+                            stepSize={0.1}
+                            majorStepSize={1}
+                        />
+                        <NumericInput
+                            key={selectedElem.id + "-z"}
+                            defaultValue={selectedElem.z}
+                            onValueChange={(val) => {
+                                !isNaN(val) && setSelectedElem({ ...selectedElem, z: val });
+                            }}
+                            fill
+                            placeholder={t("transform.z") as string}
+                            minorStepSize={0.001}
+                            stepSize={0.1}
+                            majorStepSize={1}
+                        />
+                    </ControlGroup>
+                    <ControlGroup fill>
+                        <NumericInput
+                            key={selectedElem.id + "-xScale"}
+                            defaultValue={selectedElem.xScale}
+                            onValueChange={(val) => {
+                                !isNaN(val) && setSelectedElem({ ...selectedElem, xScale: val });
+                            }}
+                            fill
+                            leftIcon="arrows-horizontal"
+                            placeholder={t("transform.xScale") as string}
+                            minorStepSize={0.01}
+                            stepSize={0.1}
+                            majorStepSize={1}
+                        />
+                        <NumericInput
+                            key={selectedElem.id + "-yScale"}
+                            defaultValue={selectedElem.yScale}
+                            onValueChange={(val) => {
+                                !isNaN(val) && setSelectedElem({ ...selectedElem, yScale: val });
+                            }}
+                            fill
+                            leftIcon="arrows-vertical"
+                            placeholder={t("transform.yScale") as string}
+                            minorStepSize={0.01}
+                            stepSize={0.1}
+                            majorStepSize={1}
+                        />
+                    </ControlGroup>
+                    <ControlGroup fill>
+                        <NumericInput
+                            key={selectedElem.id + "-rotation"}
+                            defaultValue={selectedElem.rotation}
+                            onValueChange={(val) => {
+                                !isNaN(val) && setSelectedElem({ ...selectedElem, rotation: val });
+                            }}
+                            fill
+                            leftIcon="refresh"
+                            placeholder={t("transform.rotation") as string}
+                            minorStepSize={1}
+                            stepSize={45}
+                            majorStepSize={90}
+                        />
+                    </ControlGroup>
+                    <ButtonGroup minimal style={{ marginTop: 10 }} fill>
+                        <Button
+                            fill
+                            icon={selectedElem.properties.isLocked ? "lock" : "unlock"}
+                            text={selectedElem.properties.isLocked ? t("transform.unlock") : t("transform.lock")}
+                            onClick={() => {
+                                setSelectedElem({ ...selectedElem, properties: { ...selectedElem.properties, isLocked: !selectedElem.properties.isLocked } });
+                            }}
+                        />
+                        <Button
+                            fill
+                            icon="trash"
+                            text={t("transform.delete") as string}
+                            onClick={() => {
+                                removeElement(selectedElem.id);
+                                setSelectedID("" as GUID)
+                            }}
+                        />
+                    </ButtonGroup>
+                </>)}
             </PanelContainer>
             <MapError
                 isVisible={elemVisibility !== ElemVisibility.Visible}
--- src/components/properties/util/PanelContainer.tsx
+++ src/components/properties/util/PanelContainer.tsx
@@ -5,41 +5,44 @@ interface PanelContainerProps {
     children: React.ReactNode;
     title?: string;
     style?: React.CSSProperties;
+    isOpen?: boolean;
 }
 
 export default function PanelContainer(props: PanelContainerProps) {
     const [isOpen, setIsOpen] = React.useState(true);
 
     return (
-        <Card style={{
-            padding: 0,
-            marginTop: 10,
-            boxShadow: "none",
-            borderRadius: 0
-        }}>
-            <Button
-                fill
-                large
-                minimal
-                rightIcon={isOpen ? "chevron-up" : "chevron-down"}
-                onClick={() => setIsOpen(!isOpen)}
-                text={props.title}
-                alignText="left"
-                style={{ fontWeight: 600 }}
-            />
-            <Collapse
-                isOpen={isOpen}
-                keepChildrenMounted={true}>
+        <Collapse isOpen={props.isOpen ?? true}>
+            <Card style={{
+                padding: 0,
+                marginTop: 10,
+                boxShadow: "none",
+                borderRadius: 0
+            }}>
+                <Button
+                    fill
+                    large
+                    minimal
+                    rightIcon={isOpen ? "chevron-up" : "chevron-down"}
+                    onClick={() => setIsOpen(!isOpen)}
+                    text={props.title}
+                    alignText="left"
+                    style={{ fontWeight: 600 }}
+                />
+                <Collapse
+                    isOpen={isOpen}
+                    keepChildrenMounted={true}>
 
-                <div style={{
-                    paddingLeft: 10,
-                    paddingRight: 10,
-                    paddingBottom: 10,
-                    ...props.style
-                }}>
-                    {props.children}
-                </div>
-            </Collapse>
-        </Card>
+                    <div style={{
+                        paddingLeft: 10,
+                        paddingRight: 10,
+                        paddingBottom: 10,
+                        ...props.style
+                    }}>
+                        {props.children}
+                    </div>
+                </Collapse>
+            </Card>
+        </Collapse>
     );
 }
