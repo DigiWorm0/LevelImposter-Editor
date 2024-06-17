9cbd3e6b03928bbd938f0d3af677e99c45e4b65a
Digi <aus343@gmail.com>
2024-06-16 21:57:23 -0500

    Added ESLint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default [
+    // Recommended rules
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ignores: ["node_modules/*", "build/*"],
+        plugins: {
+            "@stylistic": stylistic
+        },
+        rules: {
+            "@stylistic/semi": ["warn", "always"],
+            "@stylistic/quotes": ["warn", "double"],
+            "@stylistic/indent": ["warn", 4],
+        }
+    }
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import eslint from "@nabla/vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,6 @@ export default defineConfig({
             }
         }),
         viteTsconfigPaths(),
+        eslint()
     ],
 });
