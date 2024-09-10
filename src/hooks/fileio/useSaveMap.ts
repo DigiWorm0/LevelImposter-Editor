17a1624fb04e0602276fd20e1e359ae25f06dc61
Digi <digiworm0@gmail.com>
2024-09-09 22:03:45 -0500

    Auto Trim Assets on Save

--- src/hooks/fileio/useSaveMap.ts
+++ src/hooks/fileio/useSaveMap.ts
@@ -1,14 +1,23 @@
-import { serializeMap } from "./useLISerializer";
-import { mapAtom } from "../map/useMap";
-import { isSavedAtom } from "./useIsSaved";
-import { saveFileFromBlob } from "../../utils/fileio/saveFileFromURL";
-import { atom, useSetAtom } from "jotai";
+import {serializeMap} from "./useLISerializer";
+import {mapAtom} from "../map/useMap";
+import {isSavedAtom} from "./useIsSaved";
+import {saveFileFromBlob} from "../../utils/fileio/saveFileFromURL";
+import {atom, useSetAtom} from "jotai";
+import {trimAssetsAtom} from "../assets/useTrimMapAssets";
 
 export const saveMapAtom = atom(null, async (get, set) => {
+    // Trim Assets before save
+    set(trimAssetsAtom);
+
+    // Serialize Map
     const map = get(mapAtom);
     const mapData = await serializeMap(map);
-    const blob = new Blob([mapData], { type: "application/levelimposter.map" });
+
+    // Save File Blob
+    const blob = new Blob([mapData], {type: "application/levelimposter.map"});
     saveFileFromBlob(blob, `${map.name}.lim2`);
+
+    // Set Saved
     set(isSavedAtom, true);
 });
 
