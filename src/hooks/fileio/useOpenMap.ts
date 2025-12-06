b35e489012e661a67c078a5d77d72a7690278453
Digi <digiworm0@gmail.com>
2025-12-06 15:58:56 -0600

    Call Map Reset on Open Map

--- src/hooks/fileio/useOpenMap.ts
+++ src/hooks/fileio/useOpenMap.ts
@@ -4,11 +4,13 @@ import {atom, useSetAtom} from "jotai";
 import openUploadDialog from "../../utils/fileio/openUploadDialog";
 import {deserializeMapFileFromBlob} from "../../utils/deserialization/deserializeMapFile";
 import {SUPPORTED_MAP_FILE_TYPES} from "../../types/amongus/Constants";
+import resetMap from "../../utils/map/resetMap";
 
 export const openMapAtom = atom(null, async (_, set) => {
     const file = await openUploadDialog(SUPPORTED_MAP_FILE_TYPES.join(","));
     const map = await deserializeMapFileFromBlob(file);
 
+    resetMap();
     set(isSavedAtom, true);
     set(mapAtom, map);
 
